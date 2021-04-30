3073e5848d6970bd9c2ead7b1ecdcae263c0bb36
Mojigot7 <ayyacine.pro@gmail.com>
2021-04-30 19:03:04 +0200

    table added to signup_form

--- signup/func_display.php
+++ signup/func_display.php
@@ -2,16 +2,31 @@
   function signup_form(&$errors){
     ?>
     <form class="" action="signup.php?action=signup" method="post">
-      <label for="pseudo">Pseudo</label>
-      <input type="text" name="pseudo" id="pseudo" value="" placeholder="parismollo">
-      <span><?php  if (check_error($errors, "pseudo")) echo $errors["pseudo"];?></span>
-      <label for="password">Password</label>
-      <input type="password" name="password" id="password" value="">
-      <span><?php  if (check_error($errors, "password")) echo $errors["password"];?></span>
-      <label for="password">Password Confirmation</label>
-      <input type="password" name="password2" id="Password Confirmation" value="">
-      <span><?php  if (check_error($errors, "password2")) echo $errors["password2"];?></span>
-
+      <table>
+        <thead>
+          <th>Sign Up</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td><label for="pseudo">Pseudo</label></td>
+            <td><input type="text" name="pseudo" id="pseudo" value="" placeholder="parismollo"></td>
+            <td><span><?php  if (check_error($errors, "pseudo")) echo $errors["pseudo"];?></span></td>
+          </tr>
+          </br>
+          <tr>
+            <td><label for="password">Password</label></td>
+            <td><input type="password" name="password" id="password" value=""></td>
+            <td><span><?php  if (check_error($errors, "password")) echo $errors["password"];?></span></td>
+          </tr>
+          </br>
+          <tr>
+            <td><label for="password">Password Confirmation</label></td>
+            <td><input type="password" name="password2" id="Password Confirmation" value=""></td>
+            <td><span><?php  if (check_error($errors, "password2")) echo $errors["password2"];?></span></td>
+          </tr>
+          </br>
+        </tbody>
+      </table>
       <button type="submit">Submit</button>
     </form>
     <?php
