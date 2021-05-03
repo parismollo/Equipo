a224c65289968b969f68581f6960dfdc5d23a134
Paris Mollo <parismollochristondis@gmail.com>
2021-05-03 13:27:33 +0200

    docs(login): login organization proposal

---
+++ login/login.php
@@ -0,0 +1,18 @@
+<?php
+  // TODO: #1 Start user session
+  // TODO: #2 Import library
+
+  // TODO: #3 IF GET PARAM
+    /*
+      a. login case --> validade login form  --> start user session
+                                                --> handle error
+      b. reset case --> destroy session --> redirect user
+      c. default -- >display login form
+    */
+    
+  // TODO #3 ELSE GET PARAM
+    /*
+      a. Check if there is a session --> display success message
+      b. If there is no session --> display login form
+    */
+?>
