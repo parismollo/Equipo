e1369207217cdd0f107f7c0a569091c297005542
Paris Mollo <parismollochristondis@gmail.com>
2021-05-14 13:36:54 +0200

    feat(create project)

--- signup/signup.php
+++ signup/signup.php
@@ -2,7 +2,7 @@
   ini_set('display_errors', 1);
   ini_set('display_startup_errors', 1);
   error_reporting(E_ALL);
-  require_once("func_display.php"); require_once("func_valid.php"); require_once("../database/user.php");
+  require_once("func_display.php"); require_once("func_valid.php"); require_once("../database/user/user.php");
   $errors = array();
   if(isset($_GET["action"])){
     switch ($_GET["action"]) {
