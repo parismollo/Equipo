2981b1e88bab4572cce6ceda23076a46d050da19
Mojigot7 <ayyacine.pro@gmail.com>
2021-05-14 18:20:32 +0200

    update password + reset session in profile added

--- profile/func_display.php
+++ profile/func_display.php
@@ -1,5 +1,5 @@
 <?php
-    function display_profile($user_info){
+    function display_profile($user_info, $errors, $valid){
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -21,33 +21,18 @@
                                         <h6 class="f-w-600"><?php echo $_SESSION["user"];?></h6>
                                     </div>
                                     <div class="card-block text-center text-white">
+                                        <a href="profile.php">My profile</a>
+                                    </div>
+                                    <div class="card-block text-center text-white">
+                                        <a href="profile.php?action=update">Update profile</a>
+                                    </div>
+                                    <div class="card-block text-center text-white">
                                         <a href="../login/login.php?action=reset">Reset session</a>
                                     </div>
                                 </div>
                                 <div class="col-sm-8">
                                     <div class="card-block">
-                                        <h6 class="m-b-20 p-b-5 b-b-default f-w-600">Information</h6>
-                                        <div class="row">
-                                            <div class="col-sm-6">
-                                                <p class="m-b-10 f-w-600">Email :</p>
-                                                <h6 class="text-muted f-w-400"><?php echo $user_info["email"];?></h6>
-                                            </div>
-                                            <div class="col-sm-6">
-                                                <p class="m-b-10 f-w-600">Username :</p>
-                                                <h6 class="text-muted f-w-400"><?php echo $user_info["pseudo"];?></h6>
-                                            </div>
-                                            <div class="col-sm-6">
-                                                <p class="m-b-10 f-w-600">Day of Birth :</p>
-                                                <h6 class="text-muted f-w-400"><?php echo $user_info["date"];?></h6>
-                                            </div>
-                                            <div class="col-sm-6">
-                                                <p class="m-b-10 f-w-600">Gender :</p>
-                                                <h6 class="text-muted f-w-400"><?php if($user_info["gender"] == "M")echo "Male" ; else echo "Female";?></h6>
-                                            </div>
-                                        </div>
-                                        <h6 class="m-b-20 m-t-40 p-b-5 b-b-default f-w-600">Projects</h6>
-                                        <div class="row">
-                                        </div>
+                                        <?php if(!empty($user_info)) basic_profile($user_info); else update_profile($errors, $valid);?>
                                     </div>
                                 </div>
                             </div>
@@ -61,8 +46,47 @@
 <?php
     }
 
-    function update_profile(){
+    function update_profile($errors, $valid){
 ?>
+<div class="">
+    <div class="">
+        <form class="" action="profile.php?action=valid_update" method="post">
+                <input type="password" name="password" placeholder="change password"/>
+                <p class="error"><?php  if (check_error($errors, "password")) echo $errors["password"];?></p>
+                <input type="password" name="password2" placeholder="confirm password"/>
+                <p class="error"><?php  if (check_error($errors, "password2")) echo $errors["password2"];?></p>
+                <p class="error"><?php  if (isset($valid)) echo $valid;?></p>
+                <button type="submit">Update password</button>
+        </form>
+    </div>
+</div>
+<?php 
+    }
+
+    function basic_profile($user_info){
+?>
+<h6 class="m-b-20 p-b-5 b-b-default f-w-600">Information</h6>
+<div class="row">
+                <div class="col-sm-6">
+                    <p class="m-b-10 f-w-600">Email :</p>
+                    <h6 class="text-muted f-w-400"><?php echo $user_info["email"];?></h6>
+                </div>
+                <div class="col-sm-6">
+                    <p class="m-b-10 f-w-600">Username :</p>
+                    <h6 class="text-muted f-w-400"><?php echo $user_info["pseudo"];?></h6>
+                </div>
+                <div class="col-sm-6">
+                    <p class="m-b-10 f-w-600">Day of Birth :</p>
+                    <h6 class="text-muted f-w-400"><?php echo $user_info["date"];?></h6>
+                </div>
+                <div class="col-sm-6">
+                    <p class="m-b-10 f-w-600">Gender :</p>
+                    <h6 class="text-muted f-w-400"><?php if($user_info["gender"] == "M")echo "Male" ; else echo "Female";?></h6>
+                </div>
+            </div>
+        <h6 class="m-b-20 m-t-40 p-b-5 b-b-default f-w-600">Projects</h6>
+    <div class="row"></div>
+</div>
 <?php 
     }
 ?>
