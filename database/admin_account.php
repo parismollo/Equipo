ae043e909efa07347a88334bb2c75b93f427cbbe
Mojigot7 <ayyacine.pro@gmail.com>
2021-05-15 23:12:42 +0200

    create tables in progress

--- database/admin_account.php
+++ database/admin_account.php
@@ -1,6 +1,29 @@
 <?php
     require_once('user.php');
 
+    /*function table_query(){
+        $query = ";";
+        return $query;
+    }
+
+    function create_tables(){
+        global $server, $user, $password, $database;
+        $connection = connect_to_db($server, $user, $password, $database);
+        if(isset($connection)){
+            $query = table_query();
+            $res = mysqli_query($connection, $query);
+            if(!$res){
+                $error = mysqli_error($connection);
+                display_error_page($error, "run_page.php");
+                exit;
+            }
+        }else{
+            display_error_page("Connection failed", "run_page.php");
+            exit;
+        }
+        mysqli_close($connection);    
+    }*/
+
     function create_admin_query($pseudo, $email, $hash_pass){
         $hash_pass = password_hash($hash_pass, PASSWORD_DEFAULT);
         $date = date("Y-m-d");
